d63c510e5832f5962e8a52d6c3b6d321dc36c677
Cesar Martinez <cecam92@hotmail.com>
2020-06-11 22:15:40 +1000

    BadgeDetails created and linked to BadgeEdit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import Badgenew from "../pages/BadgeNew";
 import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
 import BadgeEdit from '../pages/BadgeEdit'
+import BadgeDetails from '../pages/BadgeDetails'
 
 import Layout from "./Layout";
 function App() {
@@ -15,8 +16,8 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={Badgenew} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
-
           <Route component={NotFound} />
         </Switch>
       </Layout>
---
+++ src/pages/BadgeDetails.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import "../pages/styles/BadgeDetails.css";
+import confLogo from "../images/platziconf-logo.svg";
+import PageLoading from "../components/PageLoading";
+import PageError from "../components/PageError";
+import api from "./api";
+import Badge from "../components/Badge";
+import { Link } from "react-router-dom";
+
+class BadgeDetails extends React.Component {
+  state = {
+    loading: true,
+    error: null,
+    data: undefined,
+  };
+  componentWillMount() {
+    this.fetchData();
+  }
+  fetchData = async () => {
+    this.setState({
+      loading: true,
+      error: null,
+    });
+    try {
+      const data = await api.badges.read(this.props.match.params.badgeId);
+      this.setState({
+        loading: false,
+        data: data,
+      });
+    } catch (error) {
+      this.setState({
+        loading: true,
+        error: error,
+      });
+    }
+  };
+  render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
+    if (this.state.error) {
+      return <PageError error={this.state.error} />;
+    }
+    const badge = this.state.data;
+
+    return (
+      <React.Fragment>
+        <div className="BadgeDetails__hero">
+          <div className="container ">
+            <div className="row">
+              <div className="col-6">
+                <img src={confLogo} alt="Logo de la conferencia" />
+              </div>
+              <div className="col-6 BadgeDetails__hero-attendant-name">
+                <h1>
+                  {badge.firstName} {badge.lastName}
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName={badge.firstName}
+                lastName={badge.lastName}
+                email={badge.email}
+                twitter={badge.twitter}
+                jobTitle={badge.jobTitle}
+              />
+            </div>
+
+            <div className="col-6">
+              <h2>Actions</h2>
+              <Link
+                to={`/badges/${badge.id}/edit`}
+                className="btn btn-danger mb-4"
+              >
+                Edit
+              </Link>
+
+              <div>
+                <button to="/" className="btn btn-primary">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default BadgeDetails;
